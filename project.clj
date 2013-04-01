feaf2d7cd005dd9b5c6fcd33611212b6d9b72f1b
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-01 19:25:20 +0200

    Some testing in core

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject metis "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple statistic library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
+                 [incanter "1.4.1"]])
--- src/metis/core.clj
+++ src/metis/core.clj
@@ -1,1 +1,23 @@
-(ns metis.core)
+(ns metis.core
+  (:use metis.file-reader
+        metis.summarize))
+
+
+(def data
+  (map
+   #(parse-number (val %))
+   (format-data (read-file "data/LifeExpTable"))))
+
+(mean data)
+
+(median data)
+
+(first-quartile data)
+
+(third-quartile data)
+
+(variance data)
+
+(iqr data)
+
+(standard-deviation data)
