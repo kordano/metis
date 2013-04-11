09346af88df221a876489936964f68c7758fac40
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-12 00:18:57 +0200

    cleanup core

--- src/metis/core.clj
+++ src/metis/core.clj
@@ -1,23 +1,3 @@
 (ns metis.core
   (:use metis.file-reader
         metis.descriptive))
-
-
-(def data
-  (map
-   #(parse-number (val %))
-   (format-data (read-file "data/LifeExpTable"))))
-
-(mean data)
-
-(median data)
-
-(first-quartile data)
-
-(third-quartile data)
-
-(variance data)
-
-(iqr data)
-
-(standard-deviation data)
