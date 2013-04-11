664e40d998eddae93b850ae6608fe3b558fe2118
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-12 00:17:02 +0200

    add test libs

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [incanter "1.4.1"]])
+                 [midje "1.5.0"]])
