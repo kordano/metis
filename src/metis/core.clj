286911df55804192186a4dca423579b9e3f5f516
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-18 01:35:02 +0200

    added example

--- src/metis/core.clj
+++ src/metis/core.clj
@@ -1,3 +1,18 @@
 (ns metis.core
   (:use metis.file-reader
-        metis.descriptive))
+        metis.descriptive)
+  (:require [clojure.data.csv :as csv]))
+
+
+(defn five-point-summary
+  [data]
+  {:Minimum (reduce min data) :First-Quartile (first-quartile data) :Median (median data) :Third-Quartile (third-quartile data) :Maximum (reduce max data)})
+
+
+(def formated-data
+  (reduce conj
+          (map
+           #(assoc {} (key %) (parse-number (val %)))
+           (format-data (slurp "data/LifeExpTable")))))
+
+(five-point-summary (vals formated-data))
