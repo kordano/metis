25fa284e406f69d2095ffdb9fca70d0e9f4244b3
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-21 20:28:27 +0200

    add example data and functions

--- src/metis/core.clj
+++ src/metis/core.clj
@@ -6,7 +6,11 @@
 
 (defn five-point-summary
   [data]
-  {:Minimum (reduce min data) :First-Quartile (first-quartile data) :Median (median data) :Third-Quartile (third-quartile data) :Maximum (reduce max data)})
+  {:Minimum (reduce min data)
+   :First-Quartile (p-quantile data 0.25)
+   :Median (median data)
+   :Third-Quartile (p-quantile data 0.75)
+   :Maximum (reduce max data)})
 
 
 (def formated-data
@@ -15,7 +19,9 @@
            #(assoc {} (key %) (parse-number (val %)))
            (format-data (slurp "data/LifeExpTable")))))
 
-(five-point-summary (vals formated-data))
+(def data (vals formated-data))
+
+(five-point-summary data)
 
 (p-quantile data 0.5)
 
@@ -26,3 +32,38 @@
 (standard-deviation data)
 
 (pearson data '(50 60 70 80))
+
+;; Example hour compensation
+(def hour-comp-data (format-gapminder-data (slurp "data/hour_comp.csv")))
+
+(mean (filter #(not (nil? %)) (vals (hour-comp-data "Germany")))) ;; mean hour compensation in germany
+
+(def raw-hour-comp-06 (apply hash-map (flatten (map #(list (key %) ((val %) "2006")) hour-comp-data))))
+
+(def hour-comp-06 (apply hash-map (flatten (filter #(not (nil? (val %))) raw-hour-comp-06))))
+
+(apply min-key val hour-comp-06) ;; Country with lowest hour compensation
+
+(apply max-key val hour-comp-06) ;; Country with highest hour compensation
+
+(five-point-summary (vals hour-comp-06))
+
+(freq2 (vals hour-comp-06) '(0 10 20 30 40 50))
+
+
+;; Example working hours per week
+(def hours-week-data (format-gapminder-data (slurp "data/hours_week.csv")))
+
+(mean (filter #(not (nil? %)) (vals (hours-week-data "Germany")))) ;; mean working hours per week
+
+(def raw-hours-week-06 (apply hash-map (flatten (map #(list (key %) ((val %) "2006")) hours-week-data))))
+
+(def hours-week-06 (apply hash-map (flatten (filter #(not (nil? (val %))) raw-hours-week-06))))
+
+(apply min-key val hours-week-06) ;; Country with lowest working hours per week
+
+(apply max-key val hours-week-06) ;; Country with highest working hours per week
+
+(five-point-summary (vals hours-week-06))
+
+(freq2 (vals hours-week-06) '(25 27 29 31 33 35 37 39 41))
