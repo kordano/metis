aec57aef91b6cd47200ed831c2445e149d58f960
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-29 19:59:36 +0200

    adjusted some dependencies and cljsbuild configs

--- project.clj
+++ project.clj
@@ -3,7 +3,25 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  ;; CLJ source code path
+  :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/data.csv "0.1.2"]
-                 [midje "1.5.0"]])
+                 [hiccups "0.1.1"]
+                 [webfui "0.2.1"]
+                 [midje "1.5.0"]]
+
+  :plugins [[lein-cljsbuild "0.3.0"]]
+
+  ;; cljsbuild tasks configuration
+  :cljsbuild {:builds
+              [{:source-paths ["src/cljs/server"]
+                :compiler
+                {:output-to "resources/public/js/main.js"
+                 :target :nodejs
+                 :optimizations :simple
+                 :prettyprint true}}
+               {:source-paths ["src/cljs/client"]
+                :compiler
+                {:output-to "resources/public/static/cljs.js"}}]})
