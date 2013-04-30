47e1c990e1345e82bf89adbc054fa2b1a3bb579a
Konrad Kuehne <fuerstgivo@googlemail.com>
2013-04-30 14:34:09 +0200

    remove unused code

--- src/cljs/server/server.cljs
+++ src/cljs/server/server.cljs
@@ -2,6 +2,21 @@
   (:require-macros [hiccups.core :as hiccups])
   (:require [hiccups.runtime :as hiccupsrtm]))
 
+;; Helper
+(defn clj->js
+  "Recursively transforms ClojureScript maps into Javascript objects,
+   other ClojureScript colls into JavaScript arrays, and ClojureScript
+   keywords into JavaScript strings. Credit:
+   http://mmcgrana.github.com/2011/09/clojurescript-nodejs.html"
+  [x]
+  (cond
+    (string? x) x
+    (keyword? x) (name x)
+    (map? x) (.-strobj (reduce (fn [m [k v]]
+               (assoc m (clj->js k) (clj->js v))) {} x))
+    (coll? x) (apply array (map clj->js x))
+    :else x))
+
 
 (defn log [& args] (apply (.-log js/console) (map str args)))
 
@@ -13,11 +28,11 @@
   (hiccups/html
    [:html
     [:head
-     [:script {:type "text/javascript" :src "/socket.io/socket.io.js"}]
-     [:script {:type "text/javascript" :src "cljs/goog/base.js"}]
-     [:script {:type "text/javascript" :src "/socket.io/socket.io.js"}]]
-    [:body
-     [:h1 "Hello Sailor!"]]]))
+     [:link {:rel "stylesheet" :href "/style.css"}]
+     [:script {:type "text/javascript" :src "/cljs/goog/base.js"}]
+     [:script {:type "text/javascript" :src "/cljs.js"}]
+     [:script {:type "text/javascript"} "goog.require('metis.client');"]]
+    [:body]]))
 
 (defn get-main
   [req res]
