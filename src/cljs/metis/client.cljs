392703f8920784628c9684c9f88126b8ec5eba01
RagingRanek <fuerstgivo@googlemail.com>
2013-05-19 00:23:36 +0200

    add rgb manipulation controls

--- src/cljs/metis/client.cljs
+++ src/cljs/metis/client.cljs
@@ -1,4 +1,6 @@
 (ns metis.client
+  (:use [metis.repl :only [connect]]
+        [webfui.dom :only [defdom]])
   (:require [clojure.browser.repl :as repl]
             [clojure.browser.event :as event]
             [hiccups.runtime :as hiccupsrt]
@@ -38,11 +40,7 @@
 
 (defn draw-boxplot [r g b]
   (let [canvas (dom/by-id "theCanvas")
-        ctx (.getContext canvas "2d")
-        r (.round js/Math (* 255 (.random js/Math)))
-        g (.round js/Math (* 255 (.random js/Math)))
-        b (.round js/Math (* 255 (.random js/Math)))
-        ]
+        ctx (.getContext canvas "2d")]
     (do
       (set! (.-strokeStyle ctx) (rgba-string r g b 1))
       (.strokeRect ctx 185 185 30 30)
@@ -67,12 +65,46 @@
     [:p
      [:input {:type "button" :id "init-btn" :name "button1" :value "Init"}]
      [:input {:type "button" :id "draw-btn" :name "button2" :value "Draw!"}]]
-    [:p "r"
-     [:input [:type "range" :id "rRange" :min 0 :max 255]]]]))
+    [:p "r  "
+     [:input {:type "range" :name "points" :id "rRange" :min "0" :max "255" :value "0"}]
+     [:span {:id "current-r"}]]
+    [:p "g  "
+     [:input {:type "range" :name "points" :id "gRange" :min "0" :max "255" :value "0"}]
+     [:span {:id "current-g"}]]
+    [:p "b  "
+     [:input {:type "range" :name "points" :id "bRange" :min "0" :max "255" :value "0"}]
+     [:span {:id "current-b"}]]
+
+    ]))
 
 
 #_ (do
      (set! (.-innerHTML (dom/by-id "DIV1")) (init-canvas))
      (set! (.-onclick (dom/by-id "init-btn")) (fn [] (draw-background)))
-     (set! (.-onclick (dom/by-id "draw-btn")) (fn [] (draw-boxplot)))
-     )
+     (set! (.-onchange (dom/by-id "rRange"))
+           (fn []
+             (let [r-val (.-value (dom/by-id "rRange"))
+                   g-val (.-value (dom/by-id "gRange"))
+                   b-val (.-value (dom/by-id "bRange"))]
+                  (do
+                    (draw-boxplot r-val g-val b-val)
+                    (set! (.-innerHTML (dom/by-id "current-r")) r-val)))))
+     (set! (.-onchange (dom/by-id "gRange"))
+           (fn []
+             (let [r-val (.-value (dom/by-id "rRange"))
+                   g-val (.-value (dom/by-id "gRange"))
+                   b-val (.-value (dom/by-id "bRange"))]
+                  (do
+                    (draw-boxplot r-val g-val b-val)
+                    (set! (.-innerHTML (dom/by-id "current-g")) g-val)))))
+     (set! (.-onchange (dom/by-id "bRange"))
+           (fn []
+             (let [r-val (.-value (dom/by-id "rRange"))
+                   g-val (.-value (dom/by-id "gRange"))
+                   b-val (.-value (dom/by-id "bRange"))]
+                  (do
+                    (draw-boxplot r-val g-val b-val)
+                    (set! (.-innerHTML (dom/by-id "current-b")) b-val)))))
+     (set! (.-onclick (dom/by-id "draw-btn")) (fn [] (draw-boxplot))))
+
+#_(js/alert "red alert!")
